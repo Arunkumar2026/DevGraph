82cd72857b46f0da637dd31ddd67835ba41ea7af
Arun Kumar <akulakumar453@gmail.com>
2026-08-20 08:34:34 +0530

    added neo4j driver

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,27 +1,14 @@
-import neo4j from 'neo4j-driver';
-import dotenv from 'dotenv';
+import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const URI = process.env.COGNODB_URI;
-const USERNAME = process.env.COGNODB_USERNAME;
-const PASSWORD = process.env.COGNODB_PASSWORD;
-
-console.log({
-  URI: URI ? "Loaded" : "Missing",
-  USERNAME: USERNAME ? "Loaded" : "Missing",
-  PASSWORD: PASSWORD ? "Loaded" : "Missing",
-});
-
-
-if (!URI || !USERNAME || !PASSWORD){
-    throw new Error("CognoDB credentials are missing in .env");
-}
-
-
 const driver = neo4j.driver(
-    URI,
-    neo4j.auth.basic(USERNAME, PASSWORD)
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(
+    process.env.NEO4J_USERNAME,
+    process.env.NEO4J_PASSWORD
+  )
 );
 
 export default driver;
