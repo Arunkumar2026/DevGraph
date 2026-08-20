0d4336107dc5f09d823c636a3829adc391bab084
Arun Kumar <akulakumar453@gmail.com>
2026-08-20 09:46:53 +0530

    database configuration has changed to cognoDB

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,20 +1,3 @@
-// import neo4j from "neo4j-driver";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const driver = neo4j.driver(
-//   process.env.NEO4J_URI,
-//   neo4j.auth.basic(
-//     process.env.NEO4J_USERNAME,
-//     process.env.NEO4J_PASSWORD
-//   )
-// );
-
-// export default driver;
-
-
-
 import neo4j from "neo4j-driver";
 import dotenv from "dotenv";
 
